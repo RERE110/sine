127691573b0e05fe92c63f8284def3a3c3cb266c
LENGLET Rémy <lenglet.remy@atomsit.com>
2024-03-24 16:24:46 +0100

    First test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,91 @@ use Illuminate\Support\Facades\Route;
 
 
 
+function callApi($url, $data_string, $verb="GET", $headers, $json=true, $token=false) {
+
+    $curl = curl_init();
+
+    $opts = [
+        CURLOPT_URL => $url,
+        CURLOPT_POST => false,
+        CURLOPT_CUSTOMREQUEST => $verb,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => $headers,
+    ];
+
+    if($verb !== "GET") {
+        $opts[CURLOPT_POST] = true;
+        $opts[CURLOPT_POSTFIELDS] = $data_string;
+    }
+    if($json === false && $token === false){// cas ou on veut afficher un pdf (mais pas pour l'appel WS Token)
+        $opts[CURLOPT_HEADER] = true;
+    }
+
+    curl_setopt_array($curl, $opts);
+    $executionStartTime = microtime(true);
+    $response = curl_exec($curl);
+    $executionEndTime = microtime(true);
+    $seconds = $executionEndTime - $executionStartTime;
+    if($json === true || $token === true){
+        $response = json_decode($response);
+    } else {
+        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $headers = substr($response, 0, $header_size);
+        transferHeader($headers);
+        $body = substr($response, $header_size);
+        /*
+        ## Alternative 1 : contenu transformé en data-url encodé en base64, accessible depuis un lien.
+
+        $contentType = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
+        echo '<a href="data:'.$contentType.';base64,'.base64_encode($response).'" download="download.pdf">Download</a>';
+
+        ## Alternative 2 : enregistrement du contenu dans un répertoire du serveur
+
+        file_put_contents('download.pdf', $response);
+
+        */
+    }
+    curl_close($curl);
+    return $response;
+}
+
+
+function callApiGet($url, $token, $datas = null) {
+    $urls = [
+        "PROD" => [
+            "PORTAL" => "https://portal.mahalo-app.io/oauth/token",
+            "WS" => "https://api.mahalo-app.io/aboweb"
+        ],
+        "PREPROD" => [
+            "PORTAL" => "https://portal-preprod.mahalo-app.io/oauth/token",
+            "WS" => "https://api-preprod.mahalo-app.io/aboweb"
+        ],
+        "RECETTE" => [
+            "PORTAL" => "https://portal-recette.mahalo-app.io/oauth/token",
+            "WS" => "https://api-recette.mahalo-app.io/aboweb"
+        ],
+        "LOCAL" => [
+            "PORTAL" => "https://localhost:8443/aboweb-portal/oauth/token",
+            "WS" => "https://localhost:8443/aboweb-ws"
+        ]
+    ];
+    $headers = array(
+        'Authorization: BEARER '.$token,
+        'Content-type: text/html; charset=utf-8'
+    );
+
+    $url_with_datas = $url;
+
+    if($datas !== null){
+        $url_with_datas .= '?'.http_build_query($datas);
+    }
+
+    return callApi($urls['PROD']["WS"].$url_with_datas, "", "GET", $headers);
+}
+
+
 function getToken($username, $password, $json=true) {
     $urls = [
         "PROD" => [
@@ -34,9 +119,6 @@ function getToken($username, $password, $json=true) {
             "WS" => "https://localhost:8443/aboweb-ws"
         ]
     ];
-    if($json === true){ // on affiche que dans le cas ou on veut du json
-        print "<div><pre>Recuperation du token</pre></div>";
-    }
 
     $params = [
         'grant_type' => 'password',
@@ -51,11 +133,30 @@ function getToken($username, $password, $json=true) {
         'Content-Type: application/x-www-form-urlencoded'
     );
 
-    $response = callApi($urls["PORTAL"], $data_string, "POST", $headers, $json, true);
-    if($json === true){
-        print "<div><pre>TOKEN API : ".$response->access_token."</pre></div>";
-    }
-    return $response->access_token;
+    $response = callApi($urls['PROD']["PORTAL"], $data_string, "POST", $headers, $json, true);
+
+
+
+    $params = [
+        "maxResults" => 2, // champs obligatoire compris entre 1 et 100
+        "sortOrder" => 1, // permet de trier par ordre croissant (<=> 1) ou d�croissant (<=> -1) sur le sortField
+        "sortField" => "dateFinAbonnement" // permet de filtrer sur la colonne dateFin
+    ];
+
+
+    //TRAITEMENT DES CALL API
+
+    $response = callApiGet("/editeur/187/client/11574/", $response->access_token, $params);
+
+
+    $response = \Illuminate\Support\Facades\Http::post('https://www.sinemensuel.com/wp-json/uap/v2/uap-185379-185380', [
+        'email' => $response->value->email,
+        'aboweb_num' => $response->value->codeClient,
+    ]);
+
+
+
+    return $response;
 
     /*$curl = curl_init();
 
